4150282479c129634b4a8214b0a17a299ca22301
Alessio Donato Giura <alessio.giura@gmail.com>
2022-09-19 10:19:59 +0200

    Aggiunta esercizio swap.

---
+++ Alessio/ese0916.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int * doubler(int *, int);
+
+void main(){
+    // tipo int pesa 4 bytes
+    int n, check = 1;
+    int * z;
+    while(check != 0){
+        printf("Inserire il valore intero di n, oppure 0 per interrompere.\nScelta: ");
+        scanf("%d", &n);
+        doubler()
+    }
+}
